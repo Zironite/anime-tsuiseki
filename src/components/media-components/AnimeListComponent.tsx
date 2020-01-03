75ac931d89d6a7c64fd91d53556c41b08e4b341c
liron <lironpro14@gmail.com>
2020-01-03 18:34:55 +0200

    added tabs for entire list

--- src/components/media-components/AnimeListComponent.tsx
+++ src/components/media-components/AnimeListComponent.tsx
@@ -20,6 +20,21 @@ class AnimeListComponent extends Component<RouteComponentProps<Props>, State> {
                 <Tab eventKey="watching" title="Watching">
                     <AnimeCollectionComponent pageSize={10} status={GQLMediaListStatus.CURRENT} />
                 </Tab>
+                <Tab eventKey="completed" title="Completed">
+                    <AnimeCollectionComponent pageSize={10} status={GQLMediaListStatus.COMPLETED} />
+                </Tab>
+                <Tab eventKey="planning" title="Planning">
+                    <AnimeCollectionComponent pageSize={10} status={GQLMediaListStatus.PLANNING} />
+                </Tab>
+                <Tab eventKey="paused" title="Paused">
+                    <AnimeCollectionComponent pageSize={10} status={GQLMediaListStatus.PAUSED} />
+                </Tab>
+                <Tab eventKey="dropped" title="Dropped">
+                    <AnimeCollectionComponent pageSize={10} status={GQLMediaListStatus.DROPPED} />
+                </Tab>
+                <Tab eventKey="rewatching" title="Rewatching">
+                    <AnimeCollectionComponent pageSize={10} status={GQLMediaListStatus.REPEATING} />
+                </Tab>
             </Tabs>
         )
     }
